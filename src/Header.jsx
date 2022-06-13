37081837a4fb1097b8dac2b72205e4131b439e33
akanksha3008 <aka.3008>
2022-06-14 00:04:51 +0530

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import './Header.css'
+function Header() {
+    return <header>
+        <img src="https://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="Troll Face" />
+        <p>Meme Generator</p>
+    </header>
+};
+
+export default Header;
