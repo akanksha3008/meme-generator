78a0e97f4e6d5b5632cc643ab01012a5427dc928
akanksha3008 <akankshapardeshi03@gmail.com>
2022-06-14 00:25:37 +0530

    Class compoenent

--- src/MemeGenerator.jsx
+++ src/MemeGenerator.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function MemeGenerator() {
-    return <h1> Going to display meme</h1>
+class MemeGenerator extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+    render() {
+        return <h1> Going to display meme</h1>;
+    }
+
 }
 
 export default MemeGenerator;
